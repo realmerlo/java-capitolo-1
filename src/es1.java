e96c7929fe3594ef0eb1fdaf81439dc65ba23caa
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-26 17:30:35 +0200

    first commit

---
+++ src/es1.java
@@ -0,0 +1,10 @@
+public class es1 {
+    public static void main(String[] args) {
+
+        double x = 12.0;
+        double y = 15.0;
+        int z = (int) x + (int) y;
+
+        System.out.println("the result of x + y is = " + z);
+    }
+}
---
+++ src/es2.java
@@ -0,0 +1,17 @@
+public class es2 {
+    public static void main(String[] args) {
+
+        int x = 24;
+        int y = 15;
+
+        System.out.println("the sum of x + y = " + sumOf(x , y));
+
+    }
+
+    public static int sumOf (int x, int y){
+        int z;
+        z = x + y;
+        return z;
+    }
+}
+
