6e141b137c40d8d27bacd5a233a605002f7ed19e
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-26 18:00:40 +0200

    es4 added

---
+++ src/es4.java
@@ -0,0 +1,14 @@
+public class es4 {
+    public static void main(String[] args) {
+
+        int x = 24;
+        int y = 5;
+
+        System.out.println("the division of x + y = " + divOf(x , y));
+
+    }
+
+    public static double divOf (int x, int y){
+        return  (double) x / y;
+    }
+}
