4952b182887a557eb7f521040564131e70060cbb
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-26 17:42:43 +0200

    es3 added

---
+++ src/es3.java
@@ -0,0 +1,13 @@
+public class es3 {
+    public static void main(String[] args) {
+
+       String x = "precipitevolissimevolmente";
+
+        System.out.println("the lenght of string is: " + stringLen(x) );
+
+    }
+
+    public static int stringLen (String x) {
+      return x.length();
+    }
+}
