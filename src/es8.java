83da0fc4d12e409a44237692ff14abd2fa6336cb
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-27 11:27:05 +0200

    es8 added

---
+++ src/es8.java
@@ -0,0 +1,19 @@
+public class es8 {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y = 5;
+
+        System.out.println("the result of compare is: " + compare(x, y));
+
+    }
+
+    public static boolean compare (int x, int y) {
+
+        System.out.println("value of x is " + x);
+        System.out.println("value of y is " + y);
+        return x != y;
+    }
+}
+
+
