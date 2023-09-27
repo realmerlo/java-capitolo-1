ac38630cb132ace4e9e7bad5360d53d0a3ee4edd
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-27 11:07:07 +0200

    es7 added

---
+++ src/es7.java
@@ -0,0 +1,16 @@
+public class es7 {
+    public static void main(String[] args) {
+
+        char x = 'd';
+        char y = 'f';
+
+        System.out.println("the sum of x + y = " + sumOf(x , y));
+
+    }
+
+    public static int sumOf (char x, char y){
+        int z;
+        z = x + y;
+        return z;
+    }
+}
