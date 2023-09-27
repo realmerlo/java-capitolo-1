b16d574b638a4632c781a0fbc1ed6c9dfdfa39db
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-27 11:31:53 +0200

    es9 added

---
+++ src/es9.java
@@ -0,0 +1,19 @@
+public class es9 {
+    public static void main(String[] args) {
+
+        char x = 'r';
+        char y = 'g';
+
+        System.out.println("the result of compare is: " + compare(x, y));
+
+    }
+
+    public static boolean compare (char x, char y) {
+
+        System.out.println("value of x is " + x);
+        System.out.println("value of y is " + y);
+        return x != y;
+    }
+}
+
+
