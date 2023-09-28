7796522404f1968c8d4823291cf68f08bb8fef47
Riccardo Merli <riccardo.merli017@gmail.com>
2023-09-28 14:53:45 +0200

    es19 - es20 added

---
+++ src/es19.java
@@ -0,0 +1,22 @@
+public class es19 {
+    public static void main(String[] args) {
+        int num = 27;
+        printNum(num);
+
+    }
+    public static void printNum (int n) {
+        for (int x = 0; x <= n; x ++) {
+            int print = x;
+
+            System.out.println(print);
+
+            if(x == 5){
+                break;
+            }
+
+        }
+    }
+}
+//Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
+// e stampa tutti i numeri fino al valore immesso,
+// la stampa dovrà interrompersi se il valore è uguale a 5.
---
+++ src/es20.java
@@ -0,0 +1,21 @@
+public class es20 {
+    public static void main(String[] args) {
+        int num = 18;
+        printNum(num);
+
+    }
+    public static void printNum (int n) {
+
+        for (int x = 0; x <= n; x ++) {
+            int print = x;
+
+            if(x == 5) {
+                continue;
+            }
+            System.out.println(print);
+        }
+    }
+}
+//Scrivi un programma che contenga un metodo che prende in ingresso un valore numerico
+// e stampa tutti i numeri fino al valore immesso,
+// la stampa dovrà saltare il valore uguale a 5.
